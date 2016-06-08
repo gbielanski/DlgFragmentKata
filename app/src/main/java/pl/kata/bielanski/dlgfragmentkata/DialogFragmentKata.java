d99ac5130c44caf70c2077421f619d3f8b10f269
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-08 22:53:15 +0200

    Inflate UI from layout onCreateView

--- app/src/main/java/pl/kata/bielanski/dlgfragmentkata/DialogFragmentKata.java
+++ app/src/main/java/pl/kata/bielanski/dlgfragmentkata/DialogFragmentKata.java
@@ -1,8 +1,21 @@
 package pl.kata.bielanski.dlgfragmentkata;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DialogFragmentKata extends DialogFragment {
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view;
 
+		view = inflater.inflate(R.layout.dialog_fragment_kata, container);
+		return view;
+
+	}
 }
