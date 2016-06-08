0fb41fc4e7ece020506c30ec4901b670d89c23bb
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-08 22:34:50 +0200

    Add onClickListener to button

--- app/src/main/java/pl/kata/bielanski/dlgfragmentkata/MainActivity.java
+++ app/src/main/java/pl/kata/bielanski/dlgfragmentkata/MainActivity.java
@@ -2,12 +2,24 @@ package pl.kata.bielanski.dlgfragmentkata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
 
+	@Bind(R.id.button)
+	Button showDialogButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		showDialogButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
 	}
 }
