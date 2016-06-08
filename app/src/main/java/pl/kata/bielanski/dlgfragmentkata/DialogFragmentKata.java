0521dec2062b7967de82d5bb50e0551e999a8f0f
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-08 22:38:14 +0200

    Create Dialog Fragment class

---
+++ app/src/main/java/pl/kata/bielanski/dlgfragmentkata/DialogFragmentKata.java
@@ -0,0 +1,9 @@
+package pl.kata.bielanski.dlgfragmentkata;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by gbielanski on 2016-06-08.
+ */
+public class DialogFragmentKata extends DialogFragment{
+}
